3a7f77c9601e1fee0edad7bd32e5f5689cf0429e
David Dal Busco <david.dalbusco@outlook.com>
2024-10-25 09:44:04 +0200

    chore: format

--- e2e/captcha.spec.ts
+++ e2e/captcha.spec.ts
@@ -1,14 +1,14 @@
-import {testWithII} from "../src";
+import {testWithII} from '../src';
 
 testWithII.beforeEach(async ({iiPage, browser}) => {
-    const DOCKER_CONTAINER_URL = 'http://127.0.0.1:5987';
-    const DOCKER_INTERNET_IDENTITY_ID = 'rdmx6-jaaaa-aaaaa-aaadq-cai';
+  const DOCKER_CONTAINER_URL = 'http://127.0.0.1:5987';
+  const DOCKER_INTERNET_IDENTITY_ID = 'rdmx6-jaaaa-aaaaa-aaadq-cai';
 
-    await iiPage.waitReady({url: DOCKER_CONTAINER_URL, canisterId: DOCKER_INTERNET_IDENTITY_ID});
+  await iiPage.waitReady({url: DOCKER_CONTAINER_URL, canisterId: DOCKER_INTERNET_IDENTITY_ID});
 });
 
 testWithII('should sign-in with a new user when II requires a captcha', async ({page, iiPage}) => {
-    await page.goto('/');
+  await page.goto('/');
 
-    await iiPage.signInWithNewIdentity({captcha: true});
+  await iiPage.signInWithNewIdentity({captcha: true});
 });
