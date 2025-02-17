b9a997dc1186b67fb2f2dc2dae0ef2fb734aba62
David Dal Busco <david.dalbusco@outlook.com>
2025-02-17 06:20:48 +0100

    feat: migrate tailwind v4 in demo

--- demo/src/components/modal.js
+++ demo/src/components/modal.js
@@ -83,7 +83,7 @@ const showModal = () => {
         m-0
         resize-none
         border-black border-[3px] rounded-sm bg-white shadow-[5px_5px_0px_rgba(0,0,0,1)]
-        focus:outline-none"
+        focus:outline-hidden"
                 rows="7"
                 placeholder="Your diary entry"></textarea>
 
@@ -104,7 +104,7 @@ const showModal = () => {
                     </button>
     
     <button id="submitEntry"
-      class="flex items-center gap-2 border-black dark:border-lavender-blue-500 border-[3px] transition-all rounded-sm py-1 px-8 my-2 font-semibold text-white bg-lavender-blue-500 dark:bg-black shadow-[5px_5px_0px_rgba(0,0,0,1)] dark:shadow-[5px_5px_0px_#7888ff] hover:bg-lavender-blue-600 dark:hover:bg-lavender-blue-300 dark:hover:text-black active:bg-lavender-blue-400 dark:active:bg-lavender-blue-500 active:shadow-none active:translate-x-[5px] active:translate-y-[5px]">
+      class="flex items-center gap-2 border-black dark:border-lavender-blue-500 border-[3px] transition-all rounded-xs py-1 px-8 my-2 font-semibold text-white bg-lavender-blue-500 dark:bg-black shadow-[5px_5px_0px_rgba(0,0,0,1)] dark:shadow-[5px_5px_0px_#7888ff] hover:bg-lavender-blue-600 dark:hover:bg-lavender-blue-300 dark:hover:text-black active:bg-lavender-blue-400 dark:active:bg-lavender-blue-500 active:shadow-none active:translate-x-[5px] active:translate-y-[5px]">
       Submit
     </button>
                     
--- demo/vite.config.js
+++ demo/vite.config.js
@@ -1,7 +1,8 @@
 import juno from '@junobuild/vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 import {defineConfig} from 'vite';
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-  plugins: [juno({container: true})]
+  plugins: [juno({container: true}), tailwindcss()]
 });
