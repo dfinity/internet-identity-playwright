ff1fb42e9dcf313769a60980638a2dcd15254596
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-05-01 09:38:10 +0200

    build: add vitest lint rules (#45)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,9 @@
 import {default as config} from '@dfinity/eslint-config-oisy-wallet';
+import {default as vitestConfig} from '@dfinity/eslint-config-oisy-wallet/vitest';
 
 export default [
   ...config,
+  ...vitestConfig,
   {
     ignores: ['**/dist']
   }
